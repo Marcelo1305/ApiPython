8c6c44360bc21f4d7c79d10f5fb08deb4dd46d3b
Marcelo1305 <97449319+Marcelo1305@users.noreply.github.com>
2023-10-28 23:34:54 -0300

    funcao

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from datetime import datetime
+from func import calcular_horas
 
 app = Flask(__name__)
 
@@ -7,16 +9,24 @@ def api():
     return jsonify({'message': 'Olá, mundo! Esta é uma API básica em Python.'})
 
 @app.route('/api', methods=['POST'])
-def post_api():
-    data = request.get_json()  # Obtemos os dados enviados no corpo da requisição em formato JSON
+def recebe_json():
+    try:
+        # Obter os dados do corpo da requisição em formato JSON
+        data = request.get_json()
 
-    # Exemplo de manipulação dos dados
-    if 'mensagem' in data:
-        mensagem = data['mensagem']
-        resposta = f'Você enviou a mensagem: {mensagem}'
-        return jsonify({'resposta': resposta})
+        # Extrair as datas
+        data_inicial = data['dataInicial']
+        data1 = datetime.strptime(data_inicial, '%Y-%m-%d %H:%M:%S')
+        data_final = data['dataFinal']
+        data2 = datetime.strptime(data_final, '%Y-%m-%d %H:%M:%S')
+        returno = jsonify({'horas': str(calcular_horas(data1, data2))})
+        return returno
 
-    return jsonify({'erro': 'Mensagem não encontrada no corpo da requisição'}), 400
+        #return jsonify({'message': 'JSON recebido com sucesso'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
 
 
 if __name__ == '__main__':
